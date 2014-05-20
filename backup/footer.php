7c9898da894f5a3d53cde970eea791b2ddec616c
LuoCan <GeCircle@gmail.com>
2014-05-20 23:39:31 +0800

    commit for a test

---
+++ backup/footer.php
@@ -0,0 +1,17 @@
+		</div>
+    </div>
+</div>
+<footer id="footer">
+	<div class="container">
+		<div id="quotes" style="padding-top: 10px;padding-bottom: 20px;"><?php include(dirname(__file__)."/quotes.php");quotes(); ?></div>
+		&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>
+		<?php _e('0.34 Powered by <a href="http://sae.sina.com.cn/">SAE</a>'); ?>.
+	</div>
+	<div style="display:none">
+	<script src="http://v1.cnzz.com/z_stat.php?id=1000391560&web_id=1000391560" language="JavaScript"></script>
+	</div>
+</footer>
+<?php $this->footer(); ?>
+</body>
+</html>
+<!--for test-->
